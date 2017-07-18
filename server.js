9867040e21f1f5ba35b14a9d580608d366f7b55a
quanweilena <quanweilena@gmail.com>
2017-07-18 16:56:42 -0400

    signup and signin functions added

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectId;
 var bodyParser = require('body-parser');
 var bcrypt = require('bcryptjs');
+var jwt = require('jwt-simple');
 var app = express();
+
+var JWT_SECRET = 'catsmeow';
 
 var db = null;
 
@@ -30,9 +33,15 @@ app.get('/meows',function(req,res,next){
 });
 
 app.post('/meows', function(req,res,next){
+
+	var token = req.headers.authorization;
+	var user = jwt.decode(token, JWT_SECRET);
+
 	db.collection('meows', function(err, meowsCollection){
 		var newMeow = {
-			text: req.body.newMeow
+			text: req.body.newMeow,
+			user: user._id,
+			username: user.username
 		};
 		meowsCollection.insert(newMeow, {w:1}, function(err){
 		return res.send();
@@ -41,10 +50,13 @@ app.post('/meows', function(req,res,next){
 });
 
 app.put('/meows/remove', function(req,res,next){
+
+	var token = req.headers.authorization;
+	var user = jwt.decode(token, JWT_SECRET);
+
 	db.collection('meows', function(err, meowsCollection){
 		var meowID = req.body.meow._id;
-
-		meowsCollection.remove({_id: ObjectId(meowID)}, {w:1}, function(err){
+		meowsCollection.remove({_id: ObjectId(meowID), user: user._id}, {w:1}, function(err){
 		return res.send();
 		});
 	});
@@ -53,6 +65,35 @@ app.put('/meows/remove', function(req,res,next){
 app.post('/users', function(req,res,next){
 
 	db.collection('users', function(err, usersCollection){
+		bcrypt.genSalt(10, function(err, salt) {
+		    bcrypt.hash(req.body.password, salt, function(err, hash) {
+		        var newUser = {
+					username: req.body.username,
+					password: hash
+				};
+				usersCollection.insert(newUser, {w:1}, function(err){
+					return res.send();
+				});
+			});
+		});	
+	});
+});
+
+app.put('/users/signin', function(req,res,next){
+
+	db.collection('users', function(err, usersCollection){
+
+		usersCollection.findOne({username: req.body.username}, function(err, user){
+			bcrypt.compare(req.body.password, user.password, function(err, result){
+				if(result){
+					var token = jwt.encode(user, JWT_SECRET);
+					return res.json({token: token});
+				} else{
+					return res.status(400).send();
+				}
+			});
+		});
+
 		bcrypt.genSalt(10, function(err, salt) {
 		    bcrypt.hash(req.body.password, salt, function(err, hash) {
 		        var newUser = {
